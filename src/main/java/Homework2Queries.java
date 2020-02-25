7c5b67a1ab366f30ccfafa99d6423c9d3fd0ecce
AdrianAlucard <wartinator@gmail.com>
2020-02-24 20:40:13 -0500

    refactor to make it easier to add additional queries

--- src/main/java/Homework2Queries.java
+++ src/main/java/Homework2Queries.java
@@ -6,15 +6,18 @@ import org.apache.jena.reasoner.Reasoner;
 import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.sparql.core.Quad;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class Homework2Queries {
-
     private static String PREFIX = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>" +
             "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
             "PREFIX univ: <http://www.cs.ccsu.edu/~neli/university.owl#>" +
             "PREFIX vcard: <http://www.w3.org/vcard/ns#>" +
             "PREFIX ex: <http://example.org/>";
+    private static List<QueryObject> queryObjects;
+
     public static void main(String[] args) {
         Homework2Queries homework2Queries = new Homework2Queries();
         Model schema = ModelFactory.createOntologyModel();
@@ -29,20 +32,21 @@ public class Homework2Queries {
         InfModel owlSchema = ModelFactory.createInfModel(reasoner, schema);
 //        owlSchema.write(System.out, "TTL");
         System.out.println("****************** Start SELECT Queries *************************");
-        selectQuery1(owlSchema);
-        selectQuery2(owlSchema);
-        selectQuery3(owlSchema);
-        selectQuery4(owlSchema);
-        selectQuery5(owlSchema);
+        queryObjects.stream()
+                    .filter(queryObject -> queryObject.queryType.equals(QueryObject.QueryType.SELECT))
+                    .forEach(queryObject -> selectQuery(queryObject, owlSchema));
         System.out.println("****************** End SELECT Queries *************************");
 
         System.out.println("****************** Start CONSTRUCT Queries *************************");
-        constructQuery1(owlSchema);
-        constructQuery2(owlSchema);
+        queryObjects.stream()
+                    .filter(queryObject -> queryObject.queryType.equals(QueryObject.QueryType.CONSTRUCT))
+                    .forEach(queryObject -> constructQuery(queryObject, owlSchema));
         System.out.println("****************** End CONSTRUCT Queries *************************");
 
         System.out.println("****************** Start ASK Query *************************");
-        askQuery1(owlSchema);
+        queryObjects.stream()
+                    .filter(queryObject -> queryObject.queryType.equals(QueryObject.QueryType.ASK))
+                    .forEach(queryObject -> askQuery(queryObject, owlSchema));
         System.out.println("****************** End ASK Query *************************");
     }
 
@@ -50,85 +54,17 @@ public class Homework2Queries {
         return this.getClass().getResource("hw2.ttl").getFile();
     }
 
-    private static void selectQuery1(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "SELECT ?name WHERE {" +
-                            "?student a univ:Student ;" +
-                            "univ:helpsWith [a univ:Course ;" +
-                            "ex:name 'C Programming Language'];" +
-                            " foaf:name ?name}";
-        selectQuery(getQueryExecution(owlSchema, queryString),
-                    "Who are the students that can help with C Programming Language?\n");
+    public static void selectQuery(QueryObject queryObject, InfModel owlSchema) {
+        selectQuery(getQueryExecution(owlSchema, queryObject.queryString), queryObject.message);
     }
 
-    private static void selectQuery2(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "SELECT ?name WHERE {" +
-                            "?prof a univ:Professor ;" +
-                            "vcard:title 'Doctor';" +
-                            "foaf:name ?name}";
-        selectQuery(getQueryExecution(owlSchema, queryString) ,
-                    "Who are the professors who have the title of 'Doctor'?\n");
+    public static  void constructQuery(QueryObject queryObject, InfModel owlSchema) {
+        constructQuery(getQueryExecution(owlSchema, queryObject.queryString), queryObject.message);
     }
 
-    private static void selectQuery3(InfModel owlSchema) {
-        String queryString = PREFIX +
-                                "SELECT ?title ?days WHERE {" +
-                                "?course a univ:Course ;" +
-                                "ex:name ?title ." +
-                                "OPTIONAL {?course ex:days ?days}}";
-        selectQuery(getQueryExecution(owlSchema, queryString),
-                "What classes are offered and which days (if known) are they available?\n");
+    public static void askQuery(QueryObject queryObject, InfModel owlSchema) {
+        askQuery(getQueryExecution(owlSchema, queryObject.queryString), queryObject.message);
     }
-
-    private static void selectQuery4(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "SELECT ?name ?staffId WHERE {" +
-                            "?student a univ:Student ;" +
-                            "foaf:name ?name ;" +
-                            "univ:staffId ?staffId ; }";
-        selectQuery(getQueryExecution(owlSchema, queryString),
-                    "What are the names of the lab assistants and what are their univ ids?\n");
-    }
-
-    private static void selectQuery5(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "SELECT ?name ?email ?phone WHERE {" +
-                            "?prof a univ:Professor ;" +
-                            "univ:title 'Department Chair';" +
-                            "foaf:name ?name ;" +
-                            "univ:e-mail ?email ;" +
-                            "foaf:phone ?phone ;}";
-        selectQuery(getQueryExecution(owlSchema, queryString),
-                    "Who is the department chair and what is their contact info?\n");
-    }
-
-   private static void constructQuery1(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "CONSTRUCT {?student_name ex:status 'Part Time Student Staff' }" +
-                            "WHERE {?student a univ:Student ;" +
-                            "foaf:name ?student_name;" +
-                            "univ:staffId ?staffId}";
-        constructQuery(getQueryExecution(owlSchema, queryString), "Is there a graph of students who are part time staff?\n");
-   }
-
-   private static void constructQuery2(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "CONSTRUCT {?student_name ex:tutors ?course_name }" +
-                             "WHERE {?student a univ:Student ;" +
-                                "foaf:name ?student_name;" +
-                                "univ:helpsWith ?course ." +
-                                "?course ex:name ?course_name}";
-        constructQuery(getQueryExecution(owlSchema, queryString), "Is there a graph of students who tutor courses?\n");
-   }
-
-   private static void askQuery1(InfModel owlSchema) {
-        String queryString = PREFIX +
-                            "ASK {?student a univ:Student ;" +
-                            "univ:helpsWith ?course ." +
-                            "?course ex:name 'Computer Architecture'}";
-        askQuery(getQueryExecution(owlSchema, queryString), "Is there a student who can help with computer architecture?\n");
-   }
 
     /**
      * Method will execute a select query and print out the resultSet
@@ -181,5 +117,78 @@ public class Homework2Queries {
      */
     private static QueryExecution getQueryExecution(InfModel owlSchema, String queryString) {
         return QueryExecutionFactory.create(QueryFactory.create(queryString), owlSchema);
+    }
+
+    // add objects to queryobjects list
+    // using this to reduce code duplication
+    static {
+        queryObjects = new ArrayList<>();
+        queryObjects.add(new QueryObject("Who are the students that can help with C Programming Language?\n" ,
+                    PREFIX +
+                        "SELECT ?name WHERE {" +
+                        "?student a univ:Student ;" +
+                        "univ:helpsWith [a univ:Course ;" +
+                        "ex:name 'C Programming Language'];" +
+                        " foaf:name ?name}", QueryObject.QueryType.SELECT));
+        queryObjects.add(new QueryObject("Who are the professors who have the title of 'Doctor'?\n",
+                PREFIX +
+                "SELECT ?name WHERE {" +
+                "?prof a univ:Professor ;" +
+                "vcard:title 'Doctor';" +
+                "foaf:name ?name}", QueryObject.QueryType.SELECT));
+        queryObjects.add(new QueryObject("What classes are offered and which days (if known) are they available?\n",
+                PREFIX +
+                "SELECT ?title ?days WHERE {" +
+                "?course a univ:Course ;" +
+                "ex:name ?title ." +
+                "OPTIONAL {?course ex:days ?days}}", QueryObject.QueryType.SELECT));
+        queryObjects.add(new QueryObject("What are the names of the lab assistants and what are their univ ids?\n",
+                PREFIX +
+                "SELECT ?name ?staffId WHERE {" +
+                "?student a univ:Student ;" +
+                "foaf:name ?name ;" +
+                "univ:staffId ?staffId ; }", QueryObject.QueryType.SELECT));
+        queryObjects.add(new QueryObject("Who is the department chair and what is their contact info?\n",
+                PREFIX +
+                "SELECT ?name ?email ?phone WHERE {" +
+                "?prof a univ:Professor ;" +
+                "univ:title 'Department Chair';" +
+                "foaf:name ?name ;" +
+                "univ:e-mail ?email ;" +
+                "foaf:phone ?phone ;}", QueryObject.QueryType.SELECT));
+        queryObjects.add(new QueryObject("Is there a graph of students who are part time staff?\n",
+                PREFIX +
+                "CONSTRUCT {?student_name ex:status 'Part Time Student Staff' }" +
+                "WHERE {?student a univ:Student ;" +
+                "foaf:name ?student_name;" +
+                "univ:staffId ?staffId}", QueryObject.QueryType.CONSTRUCT));
+        queryObjects.add(new QueryObject("Is there a graph of students who tutor courses?\n",
+                PREFIX +
+                        "CONSTRUCT {?student_name ex:tutors ?course_name }" +
+                        "WHERE {?student a univ:Student ;" +
+                        "foaf:name ?student_name;" +
+                        "univ:helpsWith ?course ." +
+                        "?course ex:name ?course_name}", QueryObject.QueryType.CONSTRUCT));
+        queryObjects.add(new QueryObject("Is there a student who can help with computer architecture?\n",
+                PREFIX +
+                "ASK {?student a univ:Student ;" +
+                "univ:helpsWith ?course ." +
+                "?course ex:name 'Computer Architecture'}", QueryObject.QueryType.ASK));
+    }
+
+    static class QueryObject {
+        String message;
+        String queryString;
+        QueryType queryType;
+
+        enum QueryType {
+            SELECT, CONSTRUCT, DESCRIBE, ASK
+        }
+
+        QueryObject(String message, String queryString, QueryType queryType) {
+            this.message = message;
+            this.queryString = queryString;
+            this.queryType = queryType;
+        }
     }
 }
